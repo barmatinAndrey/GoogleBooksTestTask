f6b7e8af3b708c6e602b8cd8f4fb8e9534e7492b
barmatin <barmatin@vist-group.ru>
2021-01-04 17:12:41 +0700

    Tabs

---
+++ app/src/main/java/com/example/googlebookstesttask/BookListFragment.java
@@ -0,0 +1,43 @@
+package com.example.googlebookstesttask;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class BookListFragment extends Fragment {
+    private Integer counter;
+
+    public BookListFragment() {
+    }
+
+    public static BookListFragment newInstance(Integer counter) {
+        BookListFragment fragment = new BookListFragment();
+        Bundle args = new Bundle();
+        args.putInt("counter", counter);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null)
+            counter = getArguments().getInt("counter");
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_book_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
--- app/src/main/java/com/example/googlebookstesttask/BookSearchActivity.java
+++ app/src/main/java/com/example/googlebookstesttask/BookSearchActivity.java
@@ -1,14 +1,27 @@
 package com.example.googlebookstesttask;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class BookSearchActivity extends AppCompatActivity {
+    private TabLayout tabs;
+    private ViewPager2 pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_search);
+        tabs = findViewById(R.id.tabs);
+        pager = findViewById(R.id.pager);
+
+        pager.setAdapter(new ViewPagerAdapter(this));
+        new TabLayoutMediator(tabs, pager, (tab, position) -> tab.setText(getResources().getStringArray(R.array.tabs)[position])).attach();
+
+
     }
 }
